8c21627a5ee2f54a0147b85ec30eea7d150b54da
dstein8 <dstein8@emich.edu>
2026-02-16 11:21:14 -0500

    Did some nifty stuff

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -5,5 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Howdy github Project");
 		
+		System.out.println("Woah, another print statement!");
+		
 	}
 }
